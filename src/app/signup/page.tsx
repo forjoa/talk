02455b1234eec1bcf4506ab40daa999fe797721d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-16 19:35:52 +0200

    update inputs styles

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,5 @@
-import { connectToMongoDB } from "@/lib/db"
-
 export default function Register() {
-  const handleSubmit = async (e: FormData)=> {
+  const handleSubmit = async (e: FormData) => {
     'use server'
     const data = new FormData()
 
@@ -10,28 +8,59 @@ export default function Register() {
     data.append('password', e.get('password') as string)
 
     try {
-      await connectToMongoDB()
       const result = await fetch(`${process.env.URL}/api/register`, {
         method: 'POST',
-        body: data
+        body: data,
       })
 
-      console.log(result)
-      
-    } catch (error : any) {
+      console.log(result.statusText)
+    } catch (error: any) {
       console.error(error)
     }
   }
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='rounded border-2 p-8 shadow-[5px_5px_0px_0px_rgba(255,255,255)]'>
+    <main className='flex h-screen w-screen items-center justify-between'>
+      <div className='rounded border-2 p-8 shadow-[5px_5px_0px_0px_rgba(255,255,255)] w-4/5 block m-auto'>
         <h2 className='text-2xl'>Create a new account</h2>
-        <form action={handleSubmit}>
-          <input type="text" name="username" id="username" placeholder="Username" required className="w-full rounded border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255)] bg-transparent p-4 my-2"/>
-          <input type="text" name="fullname" id="fullname" placeholder="Fullname" required className="w-full rounded border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255)] bg-transparent p-4 my-2"/>
-          <input type="password" name="password" id="password" required className="w-full rounded border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255)] bg-transparent p-4 my-2"/>
-          <input type="submit" value="Sign up" className="w-full rounded border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255)] bg-white text-black p-4 my-2 cursor-pointer"/>
-        </form> 
+        <form action={handleSubmit} className="h-auto">
+          <label htmlFor='fullname' className='mt-8'>
+            Full name
+          </label>
+          <input
+            type='text'
+            name='fullname'
+            id='fullname'
+            placeholder='Fullname'
+            required
+            className='w-full rounded border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255)] bg-transparent p-4 my-2'
+          />
+          <label htmlFor='username' className='mt-8'>
+            Username
+          </label>
+          <input
+            type='text'
+            name='username'
+            id='username'
+            placeholder='Username'
+            required
+            className='w-full rounded border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255)] bg-transparent p-4 my-2'
+          />
+          <label htmlFor='password' className='mt-8'>
+            Password
+          </label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            required
+            className='w-full rounded border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255)] bg-transparent p-4 my-2'
+          />
+          <input
+            type='submit'
+            value='Sign up'
+            className='w-full rounded border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255)] bg-white text-black p-4 my-2 cursor-pointer'
+          />
+        </form>
       </div>
     </main>
   )
