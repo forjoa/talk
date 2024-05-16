3061a19073d4b459170955d783ecb55fe3567b92
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-16 17:09:17 +0200

    basic styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='rounded border-2'>
+      <div className='rounded border-2 p-8 shadow-[5px_5px_0px_0px_rgba(255,255,255)]'>
         <h2 className='text-2xl'>Login to your account</h2>
       </div>
     </main>
