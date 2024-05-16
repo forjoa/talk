28111a592c8eed5ceda73e9facc63018747ca92d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-16 14:45:50 +0200

    update login font styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='rounded border-2'>
-        <h2 className='text-2xl font-semibold'>Login to your account</h2>
+        <h2 className='text-2xl'>Login to your account</h2>
       </div>
     </main>
   )
