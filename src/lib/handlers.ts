d9389dd7bf2e68141af6a7ccd75bfd39133b968d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-16 20:44:24 +0200

    build: create login handler

--- src/lib/handlers.ts
+++ src/lib/handlers.ts
@@ -1,8 +1,13 @@
 'use server'
 import { redirect } from 'next/navigation'
-import { register } from './lib'
+import { login, register } from './lib'
 
 export const submitRegister = async (formData: FormData) => {
   await register(formData)
   redirect('/login')
+}
+
+export const submitLogin = async (formData: FormData) => {
+  await login(formData)
+  redirect('/')
 }
