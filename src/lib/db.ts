45c1b6923ca396cf1a3e60a3aa4e0a9328eb5292
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-16 17:41:43 +0200

    create connection to mongodb

---
+++ src/lib/db.ts
@@ -0,0 +1,22 @@
+import { Connection, connect } from 'mongoose'
+
+let cachedConnection: Connection | null = null
+
+export async function connectToMongoDB() {
+  if (cachedConnection) {
+    console.log('Using cached MONGODB connection')
+    return cachedConnection
+  }
+  
+  try {
+    const conn = await connect(process.env.MONGODB_URI as string)
+    cachedConnection = conn.connection
+
+    console.log('New mongodb connection established')
+    return cachedConnection
+  } catch (error) {
+    console.log(error)
+
+    throw error
+  }
+}
