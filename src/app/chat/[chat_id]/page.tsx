3dab3e11672aa5afb1d46863f524282eae853d35
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-17 18:41:17 +0200

    making use of lib function and finished UI

--- src/app/chat/[chat_id]/page.tsx
+++ src/app/chat/[chat_id]/page.tsx
@@ -1,21 +1,59 @@
 import sendIcon from '@/assets/send.svg'
+import { getConversation, getOtherUser } from '@/lib/getters'
+import { getSession } from '@/lib/lib'
 import Image from 'next/image'
 
-export default function Chat({ params }: { params: { chat_id: number } }) {
-  const chat_id = params.chat_id
+export default async function Chat({
+  params,
+}: {
+  params: { chat_id: number }
+}) {
+  const user = await getSession()
+  const currentUserID = user.user_id
+  const chatId = params.chat_id
+
+  // get conversation messages
+  const messages = await getConversation(chatId)
+  const { other_username, other_fullname } = await getOtherUser(
+    chatId,
+    currentUserID
+  )
+
   return (
     <div className='flex h-full flex-col'>
       <div className='flex h-[100px] items-center justify-between border-b border-gray-200 px-4 dark:border-gray-800'>
         <div className='flex items-center gap-3'>
           <div className='avatar'>
-            <div className='avatar-fallback'>JD</div>
+            <div className='bg-gray-600 h-10 w-10 rounded-full grid place-items-center'>
+              {other_fullname?.toString().split(' ')[0].charAt(0) +
+                '' +
+                other_fullname?.toString().split(' ')[1].charAt(0)}
+            </div>
           </div>
-          <div className='font-medium'>Jane Doe</div>
+          <div className='font-medium'>{other_fullname as string}</div>
         </div>
-        <p>Id chat {chat_id}</p>
       </div>
-      <div className='flex-1 overflow-auto p-4'>
-        <div className='space-y-4' />
+      <div className='flex-1 flex overflow-auto p-4 flex-col justify-end'>
+        {messages.map((message: any) => (
+          <div
+            key={message.message_id}
+            className={`flex ${
+              message.sender_id === currentUserID
+                ? 'justify-end'
+                : 'justify-start'
+            }`}
+          >
+            <div
+              className={`p-3 rounded-lg shadow-sm ${
+                message.sender_id === currentUserID
+                  ? 'bg-blue-500 text-white'
+                  : 'bg-gray-300 text-black'
+              }`}
+            >
+              <div className='text-sm'>{message.content}</div>
+            </div>
+          </div>
+        ))}
       </div>
       <div className='border-t border-gray-200 bg-gray-100 p-4 dark:border-gray-800 dark:bg-gray-950'>
         <form className='flex items-center gap-3 rounded-lg bg-white p-2 pr-4 shadow-sm dark:bg-gray-800'>
