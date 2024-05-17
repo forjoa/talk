ebc788af1baeca18d1a3a9e8aa89a077d92c0cde
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-17 13:38:16 +0200

    update query to show just the other information

--- src/lib/getters.ts
+++ src/lib/getters.ts
@@ -2,8 +2,8 @@ import { db } from './db'
 
 export async function getMyChats(id: number) {
   const { rows } = await db.execute({
-    sql: 'SELECT c.conversation_id, u1.user_id AS user1_id, u1.username AS user1_username, u1.fullname AS user1_fullname, u2.user_id AS user2_id, u2.username AS user2_username, u2.fullname AS user2_fullname FROM  conversations c JOIN  users u1 ON c.user1_id = u1.user_id JOIN  users u2 ON c.user2_id = u2.user_id WHERE  c.user1_id = ? OR c.user2_id = ?;',
-    args: [id, id],
+    sql: 'SELECT c.conversation_id, CASE WHEN u1.user_id = ? THEN u2.user_id ELSE u1.user_id END AS other_user_id, CASE WHEN u1.user_id = ? THEN u2.username ELSE u1.username END AS other_username, CASE WHEN u1.user_id = ? THEN u2.fullname ELSE u1.fullname END AS other_fullname FROM conversations c JOIN users u1 ON c.user1_id = u1.user_id JOIN users u2 ON c.user2_id = u2.user_id WHERE c.user1_id = ? OR c.user2_id = ?;',
+    args: [id, id, id, id, id],
   })
 
   return rows
