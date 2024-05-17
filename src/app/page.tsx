cef5e6841e94bf2113442c094770f1a642adaa1c
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-17 10:59:16 +0200

    / is not main route for chats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { getSession } from '@/lib/lib'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function Home() {
@@ -11,7 +12,10 @@ export default async function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='rounded border-2 p-8 shadow-[5px_5px_0px_0px_rgba(255,255,255)]'>
-        <h2 className='text-2xl'>Login to your account</h2>
+        <h2 className='text-2xl'>Go to my chats</h2>
+        <Link href={'/chat'} className='text-blue-500 underline'>
+          My chats
+        </Link>
       </div>
     </main>
   )
