46aac47dfac5f2e7be485f3845f35ca22f9fde50
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-17 10:41:03 +0200

    add redirection if user is not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+import { getSession } from '@/lib/lib'
+import { redirect } from 'next/navigation'
+
+export default async function Home() {
+  const session = await getSession()
+
+  if (!session) {
+    redirect('/login')
+  }
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='rounded border-2 p-8 shadow-[5px_5px_0px_0px_rgba(255,255,255)]'>
