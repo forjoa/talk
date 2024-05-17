296b63dcbab6acbdb25902d72f781ef01aa8af71
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-17 19:44:01 +0200

    update layout colors (TODO: rest colors to change)

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -31,8 +31,8 @@ export default async function ChatLayout({
 
   return (
     <div className='flex h-screen w-full flex-col md:flex-row overflow-hidden'>
-      <div className='border-r border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-950 h-0 md:h-full md:w-72 w-full'>
-        <div className='hidden md:flex h-[100px] items-center justify-between border-b border-gray-200 px-4 dark:border-gray-800'>
+      <div className='border-r-2 border-white bg-black h-0 md:h-full md:w-72 rounded w-full shadow'>
+        <div className='hidden md:flex h-[100px] items-center justify-between border-b-2 border-white px-4'>
           <div className='flex flex-col py-4 gap-2 w-full'>
             <div className='flex w-full justify-between items-center'>
               <div>Welcome, {user.username}</div>
@@ -48,7 +48,7 @@ export default async function ChatLayout({
             </div>
             <form
               action={searchHandler}
-              className='flex items-center border rounded border-gray-800 py-2 px-4'
+              className='flex items-center border-2 rounded border-white shadow py-2 px-4'
             >
               <input
                 type='text'
@@ -62,9 +62,9 @@ export default async function ChatLayout({
               </button>
             </form>
             {usersSearched.length !== 0 && (
-              <div className='absolute top-[100px] left-0 bg-gray-800 z-10 w-full md:w-[300px] rounded-b-xl'>
+              <div className='absolute top-[98px] left-0 bg-black border-2 z-10 w-full md:w-[287px] shadow rounded-b-xl'>
                 {usersSearched.map((user, index) => (
-                  <div key={index} className='p-4 border-t border-gray-700'>
+                  <div key={index} className='p-4 border-t-2 border-white'>
                     <p className='hover:underline cursor-pointer'>
                       {user.username}
                     </p>
@@ -82,7 +82,7 @@ export default async function ChatLayout({
               <Link
                 key={index}
                 href={`/chat/${chat.conversation_id}`}
-                className='flex w-full p-4 hover:underline border-b border-gray-800'
+                className='flex p-4 m-2 mr-4 rounded hover:underline border-2 border-white shadow'
               >
                 {chat.other_username as string}
               </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='rounded border-2 p-8 shadow-[5px_5px_0px_0px_rgba(255,255,255)]'>
+      <div className='rounded border-2 p-8 shadow'>
         <h2 className='text-2xl'>Go to my chats</h2>
         <Link href={'/chat'} className='text-blue-500 underline'>
           My chats
