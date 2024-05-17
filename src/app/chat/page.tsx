552c57432c4e3135fcc8c05bc6422dc6e64e7c68
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-17 13:52:02 +0200

    default page when a conversation is not selected

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,7 @@
+export default function Chat() {
+  return (
+    <div className='flex h-full flex-col items-center justify-center'>
+      <p className='text-xl'>Welcome to Talk your new favorite Next.js chat aplicacion</p>
+    </div>
+  )
+}
