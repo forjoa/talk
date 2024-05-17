cae5745388c7b0fd82287f865cc231c33a6a7432
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-17 19:12:35 +0200

    hidding layout when user enters from mobile

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -30,12 +30,12 @@ export default async function ChatLayout({
   }
 
   return (
-    <div className='grid h-screen w-full grid-cols-[300px_1fr] overflow-hidden'>
-      <div className='border-r border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-950 h-screen'>
-        <div className='flex h-[100px] items-center justify-between border-b border-gray-200 px-4 dark:border-gray-800'>
-          <div className='flex flex-col py-4 gap-2'>
+    <div className='flex h-screen w-full flex-col md:flex-row overflow-hidden'>
+      <div className='border-r border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-950 h-0 md:h-full md:w-72 w-full'>
+        <div className='hidden md:flex h-[100px] items-center justify-between border-b border-gray-200 px-4 dark:border-gray-800'>
+          <div className='flex flex-col py-4 gap-2 w-full'>
             <div className='flex w-full justify-between items-center'>
-              <div className=''>Welcome, {user.username}</div>
+              <div>Welcome, {user.username}</div>
               <form action={logout} className='grid place-items-center'>
                 <button type='submit'>
                   <Image
@@ -55,14 +55,14 @@ export default async function ChatLayout({
                 name='username'
                 id='username'
                 placeholder='Search a user'
-                className='bg-transparent outline-none'
+                className='bg-transparent outline-none flex-grow'
               />
               <button type='submit'>
                 <Image src={searchIcon} alt='Search icon' />
               </button>
             </form>
             {usersSearched.length !== 0 && (
-              <div className='absolute top-[100px] left-0 bg-gray-800 z-10 w-[300px] rounded-b-xl'>
+              <div className='absolute top-[100px] left-0 bg-gray-800 z-10 w-full md:w-[300px] rounded-b-xl'>
                 {usersSearched.map((user, index) => (
                   <div key={index} className='p-4 border-t border-gray-700'>
                     <p className='hover:underline cursor-pointer'>
@@ -74,7 +74,7 @@ export default async function ChatLayout({
             )}
           </div>
         </div>
-        <div className='h-[calc(100%-100px)] max-h-[calc(100%-100px)] overflow-y-scroll'>
+        <div className='hidden md:h-[calc(100%-100px)] max-h-[calc(100%-100px)] overflow-y-scroll'>
           {chats.length === 0 ? (
             <p className='p-4'>You don&apos;t have any chat yet</p>
           ) : (
@@ -90,7 +90,7 @@ export default async function ChatLayout({
           )}
         </div>
       </div>
-      {children}
+      <div className='flex-1'>{children}</div>
     </div>
   )
 }
