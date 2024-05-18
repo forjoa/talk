0dd2533cc80bd0858698d58cddc528eea40aae3a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-18 10:11:11 +0200

    finish styles in mobile layout

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -32,8 +32,8 @@ export default async function Chat() {
           Welcome to Talk your new favorite Next.js chat aplicacion
         </p>
       </div>
-      <div className='border-r border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-950 md:h-0 h-full md:w-72 w-full'>
-        <div className='flex h-[100px] items-center justify-between border-b border-gray-200 px-4 dark:border-gray-800'>
+      <div className='bg-black md:h-0 h-full md:w-72 w-full'>
+        <div className='flex h-[100px] items-center justify-between border-b border-white px-4'>
           <div className='flex flex-col py-4 gap-2 w-full'>
             <div className='flex w-full justify-between items-center'>
               <div>Welcome, {user.username}</div>
@@ -49,7 +49,7 @@ export default async function Chat() {
             </div>
             <form
               action={searchHandler}
-              className='flex items-center border rounded border-gray-800 py-2 px-4'
+              className='flex items-center border-2 rounded border-white shadow py-2 px-4'
             >
               <input
                 type='text'
@@ -63,9 +63,9 @@ export default async function Chat() {
               </button>
             </form>
             {usersSearched.length !== 0 && (
-              <div className='absolute top-[100px] left-0 bg-gray-800 z-10 w-full md:w-[300px] rounded-b-xl'>
+              <div className='absolute top-[98px] left-0 bg-black border-2 z-10 w-[calc(100%-10px)] shadow rounded-b-xl'>
                 {usersSearched.map((user, index) => (
-                  <div key={index} className='p-4 border-t border-gray-700'>
+                  <div key={index} className='p-4 border-t-2 border-white'>
                     <p className='hover:underline cursor-pointer'>
                       {user.username}
                     </p>
@@ -83,7 +83,7 @@ export default async function Chat() {
               <Link
                 key={index}
                 href={`/chat/${chat.conversation_id}`}
-                className='flex w-full p-4 hover:underline border-b border-gray-800'
+                className='flex p-4 m-2 mr-4 rounded hover:underline border-2 border-white shadow'
               >
                 {chat.other_username as string}
               </Link>
