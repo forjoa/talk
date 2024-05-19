c724f4749793ee80c9af41c4716f595f680b9b8b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-19 08:46:16 +0200

    update way to get db url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@libsql/client";
 
 export const db = createClient({
-  url: process.env.TURSO_DATABASE_URL as string,
+  url: process.env.TURSO_DATABASE_URL ?? 'libsql://talk-forjoa.turso.io',
   authToken: process.env.TURSO_AUTH_TOKEN as string,
 });
 
