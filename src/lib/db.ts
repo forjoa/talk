5e26d586986dc67e4f6748dda2436eb99460aa4b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-21 14:58:03 +0200

    update env variable name with NEXT_PUBLIC%

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,6 @@ import { createClient } from "@libsql/client";
 
 export const db = createClient({
   url: process.env.TURSO_DATABASE_URL ?? 'libsql://talk-forjoa.turso.io',
-  authToken: process.env.TURSO_AUTH_TOKEN as string,
+  authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN as string,
 });
 
