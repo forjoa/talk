371548ce69143ecb6bd3fc8e7a980f5f10ee5efc
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-23 22:31:24 +0200

    add link to go to your profile

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -2,13 +2,14 @@ import logoutIcon from '@/assets/logout.svg'
 import Image from 'next/image'
 import { logout } from '@/lib/lib'
 import SearchUser from '@/components/SearchUser'
+import Link from 'next/link'
 
 const NavHeader = ({ username, userId } : { username: string, userId: number}) => {
   return (
     <div className='flex h-[100px] items-center justify-between border-b border-gray-900 px-4'>
       <div className='flex flex-col py-4 gap-2 w-full'>
         <div className='flex w-full justify-between items-center'>
-          <div>Welcome, {username}</div>
+          <div>Welcome, <Link href={'/myprofile'} className='underline cursor-pointer'>{username}</Link></div>
           <form action={logout} className='grid place-items-center'>
             <button type='submit'>
               <Image
