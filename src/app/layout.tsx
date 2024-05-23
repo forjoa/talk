d6d23cfc14071e9c41a05552988facd4edeeab70
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-23 14:32:20 +0200

    build: update way metadata is loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ const jetbrains = JetBrains_Mono({ weight: ['400', '800'], subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Talk',
   description: 'Your Next.js favorite chat aplication',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
 }
 
 export default function RootLayout({
@@ -17,10 +23,6 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
-        />
         <link rel='icon' href='/chat.png' sizes='any' />
       </head>
       <body className={jetbrains.className}>{children}</body>
