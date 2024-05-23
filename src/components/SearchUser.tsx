a1401b4b6ea61dad5a94cb55736b79cebd90d160
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-23 12:12:53 +0200

    build: now you can search a user and create a conversation

--- src/components/SearchUser.tsx
+++ src/components/SearchUser.tsx
@@ -1,11 +1,17 @@
 'use client'
 import searchIcon from '@/assets/search.svg'
-import { getAllUsers } from '@/lib/getters'
+import { getAllUsers, knowConversationExists } from '@/lib/getters'
 import { getSession } from '@/lib/lib'
+import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
-export default function SearchUser() {
+export default function SearchUser({
+  currentUserID,
+}: {
+  currentUserID: number
+}) {
+  const router = useRouter()
   const [allUsers, setAllUsers] = useState<any[]>([])
   const [searchTerm, setSearchTerm] = useState('')
   const [filteredUsers, setFilteredUsers] = useState<any[]>([])
@@ -13,29 +19,34 @@ export default function SearchUser() {
   useEffect(() => {
     async function getUsers() {
       try {
-        const user = await getSession();
+        const user = await getSession()
         if (!user || !user.user_id) {
-          throw new Error('User not authenticated');
+          throw new Error('User not authenticated')
         }
-        const rows = await getAllUsers(user.user_id);
-        setAllUsers(rows);
+        const rows = await getAllUsers(user.user_id)
+        setAllUsers(rows)
       } catch (error) {
-        console.error('Failed to fetch users:', error);
+        console.error('Failed to fetch users:', error)
       }
     }
-    getUsers();
-  }, []);
-  
+    getUsers()
+  }, [])
+
   useEffect(() => {
     const filtered = allUsers.filter((user) =>
       user.username.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredUsers(filtered);
-  }, [searchTerm, allUsers]);
-  
+    )
+    setFilteredUsers(filtered)
+  }, [searchTerm, allUsers])
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value)
+  }
+
+  const createConversation = async (otherUserID: number) => {
+    const { exists, conversationId } = await knowConversationExists(currentUserID, otherUserID)
+
+    router.push(`/chat/${conversationId}`)
   }
 
   return (
@@ -57,7 +68,11 @@ export default function SearchUser() {
       {filteredUsers.length !== 0 && searchTerm && (
         <div className='absolute top-[115px] left-0 md:left-4 bg-black border-2 z-10 w-full md:w-[282px] shadow rounded-b-xl'>
           {filteredUsers.map((user, index) => (
-            <div key={index} className='p-4 border-t-2 border-white'>
+            <div
+              key={index}
+              className='p-4 border-t-2 border-white'
+              onClick={() => createConversation(user.user_id)}
+            >
               <p className='hover:underline cursor-pointer'>{user.username}</p>
             </div>
           ))}
