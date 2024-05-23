7fd0071071579d9ca292fe30be2d23034bae612f
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-23 21:58:17 +0200

    new components implementation

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from 'react'
-import logoutIcon from '@/assets/logout.svg'
-import Image from 'next/image'
-import { getSession, logout } from '@/lib/lib'
+import { getSession } from '@/lib/lib'
 import { redirect } from 'next/navigation'
 import { getMyChats } from '@/lib/getters'
-import Link from 'next/link'
-import SearchUser from '@/components/SearchUser'
+import NavHeader from '@/components/NavHeader'
+import NavList from '@/components/NavList'
 
 export default async function ChatLayout({
   children,
@@ -22,39 +20,9 @@ export default async function ChatLayout({
 
   return (
     <div className='flex h-screen w-full flex-col md:flex-row overflow-hidden'>
-      <div className='border border-gray-900 bg-black h-0 md:w-72 md:ml-4 md:my-4 md:h-[calc(100vh-2rem)] rounded w-full'>
-        <div className='hidden md:flex h-[100px] items-center justify-between border-b border-gray-900 px-4'>
-          <div className='flex flex-col py-4 gap-2 w-full'>
-            <div className='flex w-full justify-between items-center'>
-              <div>Welcome, {user.username}</div>
-              <form action={logout} className='grid place-items-center'>
-                <button type='submit'>
-                  <Image
-                    src={logoutIcon}
-                    alt='Logout icon'
-                    className='cursor-pointer'
-                  />
-                </button>
-              </form>
-            </div>
-            <SearchUser currentUserID={user.user_id}/>
-          </div>
-        </div>
-        <div className='h-[calc(100%-100px)] max-h-[calc(100%-100px)] overflow-y-auto'>
-          {chats.length === 0 ? (
-            <p className='p-4'>You don&apos;t have any chat yet</p>
-          ) : (
-            chats.map((chat, index) => (
-              <Link
-                key={index}
-                href={`/chat/${chat.conversation_id}`}
-                className='flex p-4 m-2 rounded hover:underline border border-gray-900'
-              >
-                {chat.other_username as string}
-              </Link>
-            ))
-          )}
-        </div>
+      <div className='border border-gray-900 bg-black hidden md:block md:w-72 md:ml-4 md:my-4 md:h-[calc(100vh-2rem)] rounded w-full'>
+        <NavHeader username={user.username} userId={user.user_id} />
+        <NavList chats={chats} />
       </div>
       <div className='flex-1 h-[calc(100vh-2rem)] md:my-4'>{children}</div>
     </div>
--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,8 @@
-import logoutIcon from '@/assets/logout.svg'
-import Image from 'next/image'
-import { getSession, logout } from '@/lib/lib'
+import { getSession } from '@/lib/lib'
 import { redirect } from 'next/navigation'
 import { getMyChats } from '@/lib/getters'
-import Link from 'next/link'
-import SearchUser from '@/components/SearchUser'
+import NavHeader from '@/components/NavHeader'
+import NavList from '@/components/NavList'
 
 export default async function Chat() {
   const user = await getSession()
@@ -19,42 +17,12 @@ export default async function Chat() {
     <>
       <div className='hidden md:flex h-full flex-col items-center justify-center'>
         <p className='text-xl'>
-          Welcome to Talk your new favorite Next.js chat aplicacion
+          Welcome to Talk your new favorite Next.js chat application
         </p>
       </div>
       <div className='bg-black md:h-0 h-full md:w-72 w-full'>
-        <div className='flex h-[100px] items-center justify-between border-b border-gray-900 px-4'>
-          <div className='flex flex-col py-4 gap-2 w-full'>
-            <div className='flex w-full justify-between items-center'>
-              <div>Welcome, {user.username}</div>
-              <form action={logout} className='grid place-items-center'>
-                <button type='submit'>
-                  <Image
-                    src={logoutIcon}
-                    alt='Logout icon'
-                    className='cursor-pointer'
-                  />
-                </button>
-              </form>
-            </div>
-            <SearchUser currentUserID={user.user_id}/>
-          </div>
-        </div>
-        <div className='h-[calc(100%-100px)] max-h-[calc(100%-100px)] overflow-y-scroll'>
-          {chats.length === 0 ? (
-            <p className='p-4'>You don&apos;t have any chat yet</p>
-          ) : (
-            chats.map((chat, index) => (
-              <Link
-                key={index}
-                href={`/chat/${chat.conversation_id}`}
-                className='flex p-4 m-2 mr-4 rounded hover:underline border border-gray-900'
-              >
-                {chat.other_username as string}
-              </Link>
-            ))
-          )}
-        </div>
+        <NavHeader username={user.username} userId={user.user_id} />
+        <NavList chats={chats} />
       </div>
     </>
   )
